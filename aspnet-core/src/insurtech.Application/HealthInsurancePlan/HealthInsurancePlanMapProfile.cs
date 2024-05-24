1fd8eceb22574ae864862d760075fa444aeaeebd
HabibaShata <156047923+HabibaShata@users.noreply.github.com>
2024-05-25 00:59:31 +0300

    add health insurance plan

---
+++ aspnet-core/src/insurtech.Application/HealthInsurancePlan/HealthInsurancePlanMapProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insurtech.HealthInsurancePlan
+{
+    public class HealthInsurancePlanMapProfile:Profile
+    {
+        public HealthInsurancePlanMapProfile()
+        {
+            CreateMap<insurtech.Models.HealthInsurancePlan, HealthInsurancePlanDTO.HealthInsurancePlanDTO>().ReverseMap();
+        }
+    }
+}
