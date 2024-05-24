513643caf875fc9e39a962b1a01c7bc50ef99524
AsmaaAlbhbity <asmaa.albhbity@gmail.com>
2024-05-25 02:20:11 +0300

    Create Map Profile

--- aspnet-core/src/insurtech.Application/HomeInsurancePlan/DTO/HomeInsurancePlanMapProfile.cs
+++ aspnet-core/src/insurtech.Application/HomeInsurancePlan/DTO/HomeInsurancePlanMapProfile.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using insurtech.Models;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace insurtech.HomeInsurancePlan.DTO
 {
-    internal class HomeInsurancePlanMapProfile
+    public class HomeInsurancePlanMapProfile : Profile
     {
+        public HomeInsurancePlanMapProfile()
+        {
+            CreateMap<insurtech.Models.HomeInsurancePlan,AddEditHomeInsurancePlanDTO>().ReverseMap();
+
+            CreateMap<insurtech.Models.HomeInsurancePlan, HomeInsurancePlanListDTO>()
+                .ForMember(a => a.CompanyName, b => b.MapFrom(b => b.Company.Name))
+                .ForMember(c => c.CategoryName, b => b.MapFrom(b => b.Category.Name));
+        }
     }
 }
