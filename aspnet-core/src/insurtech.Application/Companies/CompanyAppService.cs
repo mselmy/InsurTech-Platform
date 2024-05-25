ef55593ddbbda7c7a82436ff7e2481ba451c1634
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-05-25 03:20:37 +0300

    chore: Add CompanyAppService and related classes for managing companies

---
+++ aspnet-core/src/insurtech.Application/Companies/CompanyAppService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using insurtech.Companies.Dto;
+using insurtech.Models;
+
+namespace insurtech.Companies
+{
+    public class CompanyAppService : AsyncCrudAppService<Company, CompanyDto, long, PagedAndSortedResultRequestDto, CreateCompanyInput, CompanyDto>, ICompanyAppService
+    {
+        private readonly IRepository<Company, long> _companyRepository;
+        CompanyAppService(IRepository<Company, long> companyRepository) : base(companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+        public void CreateCompany(CreateCompanyInput input)
+        {
+           var company = ObjectMapper.Map<Company>(input);
+
+          
+            _companyRepository.Insert(company);
+        }
+
+    }
+}
---
+++ aspnet-core/src/insurtech.Application/Companies/ICompanyAppService.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using insurtech.Companies.Dto;
+using Abp.Application.Services.Dto;
+
+namespace insurtech.Companies
+{
+    public interface ICompanyAppService : IAsyncCrudAppService<CompanyDto, long, PagedAndSortedResultRequestDto, CreateCompanyInput, CompanyDto>
+    {
+        void CreateCompany(CreateCompanyInput input);
+    }
+}
