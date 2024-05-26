d797670736345971f8cae5b818d8a47083fd0d3f
EbrahimDawoud <ebrahim_EN30895331@f-eng.tanta.edu.eg>
2024-05-26 17:37:12 +0300

    refactor: add validation for registeration

---
+++ aspnet-core/src/insurtech.Application/Companies/Dto/PagedCompanyResultRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services.Dto;
+using insurtech.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insurtech.Companies.Dto
+{
+    //custom PagedResultRequestDto
+    public class PagedCompanyResultRequestDto : PagedResultRequestDto
+    {
+      public CompanyStatus? Status { get; set; }
+    }
+}
