c95689fcfe46232b7b6bd9aa87fefa74d1067e24
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-05-26 16:01:56 +0300

    chore: Update HttpClientModule import in root module

---
+++ angular/src/app/services/registration-requests-service.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationRequestsServiceService {
+  private baseUrl = 'https://localhost:44311/';
+  constructor() { }
+}
